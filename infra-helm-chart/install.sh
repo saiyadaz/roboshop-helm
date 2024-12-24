bc8573377112bee3ce51ad2adda9e9e6440f363a
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-12-24 15:24:49 -0500

    addd roboshop helm-new

---
+++ infra-helm-chart/install.sh
@@ -0,0 +1,26 @@
+aws eks update-kubeconfig --name dev-eks
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm upgrade -i ngx-ingres ingress-nginx/ingress-nginx -f ingress.yaml
+
+
+LOAD_BALANCER=$(kubectl get svc ngx-ingres-ingress-nginx-controller | grep ngx-ingres-ingress-nginx-controller | awk '{print $4}')
+
+while  true ; do
+  echo "Waiting for Load balancer to come to Active"
+  nslookup $LOAD_BALANCER &>/dev/null
+  if [ $? -eq 0 ]; then break ; fi
+  sleep 5
+
+done
+
+exit
+kubectl create ns argocd
+kubectl apply -f argo-dev.yaml -n argocd
+
+kubectl apply -f external-dns-dev.yaml
+
+while true ; do
+  argocd admin initial-password -n argocd &>/dev/null
+  if [ $? -eq 0 ]; then break ; fi
+  sleep 5
+done
