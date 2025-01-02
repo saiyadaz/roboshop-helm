1035bfa90f9604a7c91bcdcd254964528422d231
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2025-01-02 02:15:02 -0500

    editin helm1 to helm

--- infra-helm-chart/argocd.sh
+++ infra-helm-chart/argocd.sh
@@ -2,6 +2,6 @@
   argocd login $(kubectl get svc -n argocd argocd-server | awk '{print $4}' | tail -1) --username admin --password$(argocd admin initial-password -n argocd | head -1) --insecure --grpc-web
 
   for app in backend frontend catalogue user cart dispatch payment shipping; do
-     argocd app create ${app}  --repo https://github.com/saiyadaz/roboshop-helm1 --path chart --dest-namespace default --dest-server https://kubernetes.default.svc --grpc-web --values values/${app}.yaml
+     argocd app create ${app}  --repo https://github.com/saiyadaz/roboshop-helm --path chart --dest-namespace default --dest-server https://kubernetes.default.svc --grpc-web --values values/${app}.yaml
      argocd app sync ${app}
   done
