3f2d486be0a094d00a51fc648939531cd45e1020
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2025-01-06 12:11:31 -0500

    edit, change the install.sh edit it add elk

--- infra-helm-chart/install.sh
+++ infra-helm-chart/install.sh
@@ -1,27 +1,59 @@
-
 aws eks update-kubeconfig --name dev-eks
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm upgrade -i ngx-ingres ingress-nginx/ingress-nginx -f ingress.yaml
 
-
 LOAD_BALANCER=$(kubectl get svc ngx-ingres-ingress-nginx-controller | grep ngx-ingres-ingress-nginx-controller | awk '{print $4}')
 
-while  true ; do
-  echo "Waiting for Load balancer to come to Active"
+while true ; do
+  echo "Waiting for Load Balancer to come to Active"
   nslookup $LOAD_BALANCER &>/dev/null
   if [ $? -eq 0 ]; then break ; fi
   sleep 5
-
 done
 
-exit
+kubectl apply -f external-dns-dev.yaml
+sleep 15
+
 kubectl create ns argocd
 kubectl apply -f argo-dev.yaml -n argocd
 
-kubectl apply -f external-dns-dev.yaml
 
 while true ; do
   argocd admin initial-password -n argocd &>/dev/null
   if [ $? -eq 0 ]; then break ; fi
   sleep 5
 done
+
+
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+
+helm upgrade -i pstack prometheus-community/kube-prometheus-stack -f pstack-dev.yaml
+
+# grafana default username / password - admin / prom-operator
+
+kubectl create -f https://download.elastic.co/downloads/eck/2.13.0/crds.yaml
+kubectl apply -f https://download.elastic.co/downloads/eck/2.13.0/operator.yaml
+helm upgrade -i elk elastic/eck-stack -n elastic-stack --create-namespace
+
+ES_PASSWORD=$(kubectl get secrets -n elastic-stack elasticsearch-es-elastic-user -o json | jq '.data.elastic' | sed -e 's/"//g' | base64 --decode)
+sed -e "s/ES_PASSWORD/${ES_PASSWORD}/" eck.yaml >/tmp/eck.yaml
+kubectl apply -f /tmp/eck.yaml
+
+helm repo add elastic https://helm.elastic.co
+helm upgrade -i filebeat elastic/filebeat -f filebeat.yml
+
+helm repo add autoscaler https://kubernetes.github.io/autoscaler
+helm upgrade -i node-autoscaler autoscaler/cluster-autoscaler --set 'autoDiscovery.clusterName'=dev-eks
+
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+
+
+## End
+echo
+echo
+echo
+echo "ArgoCD Password : $(argocd admin initial-password -n argocd | head -1)"
+echo "Grafana Username / Password : admin  / prom-operator"
+echo "Elastic Username / Password : elastic  / $(kubectl get secrets -n elastic-stack elasticsearch-es-elastic-user -o json | jq '.data.elastic' | sed -e 's/"//g' | base64 --decode)"
+
