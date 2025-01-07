f4386d65a5e80874b53a6c10c54966591cf08d75
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2025-01-07 04:11:04 -0500

    edit it1

--- argocd.sh
+++ argocd.sh
@@ -1,6 +1,5 @@
   kubectl create secret generic vault-token --from-literal=token=hvs.dpdlNoMWMOl4YHgDgIkK5FsJ
-  argocd login $(kubectl get svc -n argocd argocd-server | awk '{print $4}' | tail -1) --username admin --password$(argocd admin initial-password -n argocd | head -1) --insecure --grpc-web
-
+  argocd login $(kubectl get ingress -A | grep argocd | awk '{print $4}') --username admin --password $(argocd admin initial-password -n argocd | head -1) --insecure --grpc-web
   for app in  frontend catalogue user cart dispatch payment shipping; do
      argocd app create ${app}  --repo https://github.com/saiyadaz/roboshop-helm --path chart --dest-namespace default --dest-server https://kubernetes.default.svc --grpc-web --values values/${app}.yaml
      argocd app sync ${app}
